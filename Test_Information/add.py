4f0fbe24b5a270803a283c1ceeb9e00cedbd1d6d
Sheema parveen <sheemaparveenfeb14@gmail.com>
2023-08-15 16:31:32 +0530

    All the values used in project are stored here

---
+++ Test_Information/add.py
@@ -0,0 +1,29 @@
+#add employee
+class Add:
+    #employee1
+    firstName ="Aahamed"
+    middleName = "shahiq"
+    lastName = "M" 
+    employee_id = "1234" 
+    Username ="shahiqhhjk" #5char
+    password = "shahiq27" #7char
+    confirm_password = "shahiq27" #7char min1 num
+    status= "status_disabled"
+    birth="1998-02-14"
+    gender = "gendermale"
+    del_user="Aahamed shahiq M"
+    cityname="chennai"
+    statename = "TamilNadu"
+    countryname="India"
+    number="987654321"
+    mail ="shahiqm@gmail.com"
+    
+    marital_status="Single"
+    sin_no="78906540"
+    ssn_no="54326789"
+    nick_name="babydoll"
+    license_no="34567890"
+    exp_date="2025-12-30"
+    
+   
+    
---
+++ Test_Information/invalid_info.py
@@ -0,0 +1,4 @@
+class Invalid_info:
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username = "Admin"
+    password = "Invalid password"
